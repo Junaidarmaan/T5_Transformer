e507f449d8d28584047fa58313bd31baf5c7237a
junaid <junnubest@gmail.com>
2025-06-14 20:41:14 +0530

    shifted to t5-base try 1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from transformers import pipeline
 
 app = Flask(__name__)
-pipe = pipeline("text2text-generation", model="t5-small", tokenizer="t5-small")
+pipe = pipeline("text2text-generation", model="t5-base", tokenizer="t5-base")
 
 @app.route("/generate", methods=["POST"])
 def generate():
